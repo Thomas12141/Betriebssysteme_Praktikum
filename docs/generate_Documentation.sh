ef102be97ebc86d819bf810e265ec9f83418b9e6
elias-wsl <es724040@fh-muenster.de>
2024-03-23 15:39:15 +0100

    fix osmp_size docu + add a version to the doxygen pdf

--- docs/generate_Documentation.sh
+++ docs/generate_Documentation.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#========== Check for dependencies [start] ==========#
 # Check if doxygen is installed
 if ! command -v doxygen >/dev/null 2>&1; then
     echo "doxygen could not be found (install with: `sudo apt-get install doxygen`)"
@@ -23,29 +24,44 @@ if ! command -v dot >/dev/null 2>&1; then
     echo "Graphviz could not be found (install with: `sudo apt-get install graphviz`)"
     exit 1
 fi
+#========== Check for dependencies [end]   ==========#
 
+
+#========== CWD GUARD [start] ==========#
 PROJECT_DIR=$(realpath "$(dirname "$0")/..")
 CALL_DIR=$(pwd)
 
 # Change to the project directory
 cd "$PROJECT_DIR" || exit 1
+#========== CWD GUARD [end]   ==========#
+
+
+#========== Variables [start] ==========#
+DOXYFILE="./docs/Doxyfile"
+PDF_NAME="OSMP_Documentation"
+#========== Variables [end]   ==========#
+
 
 # Create the doxygen documentation
-doxygen ./docs/Doxyfile
+export PROJECT_NUMBER="Erstellt am $(date +'%d.%m.%Y') um $(date +'%H:%M:%S')."
+doxygen ${DOXYFILE}
 
-cd ./docs/latex || exit 1
+cd ./docs/latex || { rm -r ./latex; rm -r ./html; exit 1; }
 
 # Create the pdf documentation
-make
+echo "--- Creating the pdf documentation ---"
+make > /dev/null || { cd ..; rm -r ./latex; rm -r ./html; exit 1; }
 
 # Move the pdf documentation to the docs directory
-mv ./refman.pdf ../OSMP_Documentation.pdf
+mv ./refman.pdf ../$PDF_NAME.pdf
 
 # Delete unnecessary files
 cd ..
 rm -r ./latex
-# Maybe we want to keep the html documentation
 rm -r ./html
 
+
+#========== CWD GUARD [start] ==========#
 # Change back to the call directory
 cd "$CALL_DIR" || exit 1
+#========== CWD GUARD [end]   ==========#
