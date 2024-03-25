15b103e3eccb4e4ca8fd18e5e9f3fc07b2f09dd5
elias steffen - MB <elias.steffen@dr-steffen.com>
2024-03-25 11:18:00 +0100

    remove time from "version"

--- docs/generate_Documentation.sh
+++ docs/generate_Documentation.sh
@@ -3,25 +3,25 @@
 #========== Check for dependencies [start] ==========#
 # Check if doxygen is installed
 if ! command -v doxygen >/dev/null 2>&1; then
-    echo "doxygen could not be found (install with: `sudo apt-get install doxygen`)"
+    echo "doxygen could not be found (try to install with: sudo apt-get install doxygen)"
     exit 1
 fi
 
 # Check if make is installed
 if ! command -v make >/dev/null 2>&1; then
-    echo "make could not be found (install with: `sudo apt-get install make`)"
+    echo "make could not be found (try to install with: sudo apt-get install make)"
     exit 1
 fi
 
 # Check if pdflatex is installed
 if ! command -v pdflatex >/dev/null 2>&1; then
-    echo "pdflatex could not be found (install with: `sudo apt-get install texlive-latex-base`)"
+    echo "pdflatex could not be found (try to install with: sudo apt-get install texlive-latex-base)"
     exit 1
 fi
 
 # Check if dot (part of Graphviz) is installed
 if ! command -v dot >/dev/null 2>&1; then
-    echo "Graphviz could not be found (install with: `sudo apt-get install graphviz`)"
+    echo "Graphviz could not be found (try to install with: sudo apt-get install graphviz)"
     exit 1
 fi
 #========== Check for dependencies [end]   ==========#
@@ -43,7 +43,7 @@ PDF_NAME="OSMP_Documentation"
 
 
 # Create the doxygen documentation
-export PROJECT_NUMBER="Erstellt am $(date +'%d.%m.%Y') um $(date +'%H:%M:%S')."
+export PROJECT_NUMBER="Erstellt am $(date +'%d.%m.%Y')."
 doxygen ${DOXYFILE}
 
 cd ./docs/latex || { rm -r ./latex; rm -r ./html; exit 1; }
