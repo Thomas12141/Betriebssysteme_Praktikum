98bfd3676b041c51bef6cce8fe69d258e8fa03b6
Alina Rölver <ar723802@fh-muenster.de>
2024-04-14 16:13:03 +0200

    Use variable to satsify compiler

--- src/osmp_executables/osmpExecutable_Barrier.c
+++ src/osmp_executables/osmpExecutable_Barrier.c
@@ -17,16 +17,17 @@ int main(int argc, char *argv[]) {
     rv = OSMP_Rank(&rank);
 
     // Seed für Zufallszahlen setzen
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
     // Setze wait_time auf eine zufällige Zahl zwischen 1 und 10
     int min = 1;
     int max = 10;
     wait_time = min + rand() % (max - min + 1);
     // Warte eine zufällige Zeitspanne
     printf("Prozess %d wartet %d Sekunden...\n", getpid(), wait_time);
-    sleep(wait_time);
+    sleep((unsigned int)wait_time);
     // Rufe OSMP_Barrier(), um zu warten, bis alle anderen Prozesse ebenfalls an diesem Punkt angelangt sind.
     rv = OSMP_Barrier();
     rv = OSMP_Finalize();
+    printf("rv = %d\n", rv);
     return 0;
 }
