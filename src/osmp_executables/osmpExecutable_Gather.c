d330e78213a9e10e40a00808808f6d5e7cc225c7
Alina Rölver <ar723802@fh-muenster.de>
2024-05-20 16:15:37 +0200

    Fix gather test executable

--- src/osmp_executables/osmpExecutable_Gather.c
+++ src/osmp_executables/osmpExecutable_Gather.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "../osmp_library/OSMP.h"
 
@@ -15,17 +16,24 @@ int main(int argc, char *argv[]) {
     rv = OSMP_Size(&size);
     rv = OSMP_Rank(&rank);
     long bufin[1], bufout[size];
+    memset(bufout, '\0', sizeof(long) * (unsigned long) size);
     int recv;
     if (size < 2) {
         printf("Mindestens 2 Prozesse für Gather-Kommunikation benötigt! Vorhandene Prozesse: %d\n", size);
         exit(EXIT_FAILURE);
     }
-    recv = 1;
+    if (rank > 0) {
+        // OSMP-Prozesse > 0 sind sendende Prozesse
+        recv = 0;
+    } else {
+        // OSMP-Prozess 0 ist Root-Prozess (Empfänger)
+        recv = 1;
+    }
     // Verwende eigene PID als Payload
     bufin[0] = (long)getpid();
     // Sende/Empfange mit OSMP_Gather
     rv = OSMP_Gather(bufin, 1, OSMP_LONG, bufout, size, OSMP_LONG, recv);
-    if (recv== rank) {
+    if (recv) {
         printf("OSMP process %d received %d messages via Gather:\n", rank, size);
         for(int i=0; i<size; i++) {
             printf("[%3d] %ld\n", i, bufout[i]);
