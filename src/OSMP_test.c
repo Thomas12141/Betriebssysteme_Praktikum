80b1f608e5b263eda058af1fd75ac7799e392e57
Thomas Fidorin <tf670992@fh-muenster.de>
2024-05-26 19:58:44 +0200

    Test running 500 times osmp_run parallel.

---
+++ src/OSMP_test.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+//
+// Created by fidor on 22.05.2024.
+//
+int main(void) {
+    int count = 500;
+    errno = 0;
+
+    for (int i = count; i >= 0; --i) {
+        int pid = fork();
+        if (pid == 0) {
+            // Child process
+            printf("Child process with i = %d\n", i);
+            execl("./cmake-build-debug/osmp_run", "./cmake-build-debug/osmp_run", "3", "./cmake-build-debug/osmpExecutable_Barrier", "Test", NULL);
+            // If execl fails, print error and exit
+            perror("execl failed");
+            exit(errno);
+        } else if (pid < 0) {
+            // Fork failed
+            perror("fork failed");
+            exit(errno);
+        } else {
+            // Parent process
+            printf("Parent process created a child with i = %d\n", i);
+        }
+    }
+
+    // Parent process waits for all child processes to finish
+    for (int i = count; i >= 0; --i) {
+        wait(NULL);
+    }
+
+    return 0;
+}
